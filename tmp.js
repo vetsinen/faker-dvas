fa74dbb85e721204a38cad4bbca300030cf74cd3
vetsinen <vetsinen@gmail.com>
2021-08-11 18:21:59 +0300

    initial commit

---
+++ tmp.js
@@ -0,0 +1,7 @@
+const faker = require("faker")
+p = faker.git.shortSha()
+console.log(p)
+// o={}
+// o.licence_plates.licence_plate_read.plate_read="UI3532"
+// //
+// console.log(faker.date.between('2021-05-01','2021-08-10'))
